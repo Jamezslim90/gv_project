6969784587d235b69c114cf131e29c69a5069230
Sunday James <jamezslim90@gmail.com>
2023-05-31 20:10:13 +0100

    allowed host

--- gv_project/settings.py
+++ gv_project/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "gv-project.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "gv-platform.herokuapp.com"]
 
 
 # Application definition
