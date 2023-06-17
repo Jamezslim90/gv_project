7521f9b5cc4091705b3b672230af865d788ffa99
Sunday James <jamezslim90@gmail.com>
2023-06-17 10:23:13 +0100

    Change Daphne server to gunicorn

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-web: daphne gv_project.asgi:application --port $PORT --bind 0.0.0.0 -v2
-celery: celery -A gv_project.celery worker -l info
-celerybeat: celery -A gv_project beat -l INFO 
+web: gunicorn gv_project.wsgi --log-file -
--- gv_project/settings.py
+++ gv_project/settings.py
@@ -563,3 +563,8 @@ DATE_FORMAT = "d/m/Y"
 
 
 # }
+
+
+# web: daphne gv_project.asgi:application --port $PORT --bind 0.0.0.0 -v2
+# celery: celery -A gv_project.celery worker -l info
+# celerybeat: celery -A gv_project beat -l INFO 
